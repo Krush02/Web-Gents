baaa8d116b7f3d7a3d28b22bdc801296f0ae35e7
krush02 <Nero12$$>
2018-02-05 12:22:17 -0500

    work on design/layout, navbar, & landing page

--- resources/js/smooth-scroll.js
+++ resources/js/smooth-scroll.js
@@ -44,3 +44,23 @@
 }(document, jQuery));
 
 $('html').smoothScroll(700);
+
+var main = function() {
+
+  // ===== Scroll to Top ====
+  "use strict";
+  $(window).scroll(function() {
+      if ($(this).scrollTop() >= 800) {       // If page is scrolled more than 400px
+          $('#return-to-top').fadeIn(200);    // Fade in the arrow
+      } else {
+          $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+      }
+  });
+  $('#return-to-top').click(function() {      // When arrow is clicked
+      $('body,html').animate({
+          scrollTop : 0                       // Scroll to top of body
+      }, 500);
+  });
+};
+
+$(document).ready(main);
